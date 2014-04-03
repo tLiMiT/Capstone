1ad01fa6c938b167aa720645abec916868146230
Becker <studentoflife96@gmail.com>
2014-04-02 20:34:25 -0400

    Altered default char_order

--- capstone/program/CharacterFreqLogger.py
+++ capstone/program/CharacterFreqLogger.py
@@ -10,16 +10,15 @@ csv_suffix = '.csv'
 bin_prefix = 'CharFreqBinary_f_'
 bin_extension = '.bin'
 
+directory = ''
+
 half_life = 16000.0
 max_mem = 3
 
-CHAR_ORDER = ['\n', ' ', '!', '"', '#', '$', '%', '&', "'", 
-	'(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', 
-	'3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', 
-	'>', '?', '@', 'C', '[', '\\', ']', '^', '_', '`', 'a', 
-	'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-	'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-	'x', 'y', 'z', '{', '|', '}', '~']
+CHAR_ORDER = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+	'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+	'Y', 'Z', ',', '.', "'", '?', '(', ')', ':', ';', '!', '/', ' ']
 
 char_order = CHAR_ORDER
 
@@ -61,7 +60,7 @@ def saveCharOrder(filename, encode=True):
 	
 
 def makefilename(base):
-	return bin_prefix + base + bin_extension
+	return directory + bin_prefix + base + bin_extension
 
 ##############################################################
 ##################### CSV Reader/Writer ######################
